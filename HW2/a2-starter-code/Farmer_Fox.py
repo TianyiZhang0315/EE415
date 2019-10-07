158897465df7098423ec849c1c3251d11499eb38
tianyi zhang <zhang506@uw.edu>
2019-10-07 13:34:41 -0700

    add HW2

---
+++ HW2/a2-starter-code/Farmer_Fox.py
@@ -0,0 +1,16 @@
+'''Farmer_Fox.py
+by John Nathan Smith
+UWNetID: jnsmith98
+Student number: 9876543
+
+Assignment 2, in CSE 415, Autumn 2019.
+ 
+This file contains my problem formulation for the problem of
+the Farmer, Fox, Chicken, and Grain.
+'''
+
+# Put your formulation of the Farmer-Fox-Chicken-and-Grain problem here.
+# Be sure your name, uwnetid, and 7-digit student number are given above in 
+# the format shown.
+
+pass  # replace this.
